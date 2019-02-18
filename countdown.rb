66230cdf9f0d221dedacbe595d281e97fa0485e7
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-18 20:55:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,12 @@
 #write your code here
 
 def countdown(num)
-  
+  i = num
   while num > 0
-    puts "#{num} SECOND(S)!"
-    num-=1
+    puts "#{i} SECOND(S)!"
+    i-=1
   end
-  
+  if num = 12
+    puts "HAPPY NEW YEAR!"
+  end
 end
